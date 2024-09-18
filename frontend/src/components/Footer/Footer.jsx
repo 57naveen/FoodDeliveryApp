c40504706b8db38f3f35d36e60a65954eb077e5e
57naveen <naveenkishore65@gmail.com>
2024-09-18 11:38:59 +0530

    upto login page

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+import './Footer.css'
+import { assets } from '../../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='footer' id='footer'>
+        <div className="footer-content">
+          <div className="footer-content-left">
+              <img src={assets.logo} className='footer-logo' alt="" />
+              <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.is enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatisde mollis pretium. Integer tincidunt. </p>
+              <div className="footer-social-icons">
+                <img src={assets.facebook_icon} alt="" />
+                <img src={assets.twitter_icon} alt="" />
+                <img src={assets.linkedin_icon} alt="" />
+              </div>
+          </div>
+          <div className="footer-content-center">
+              <h2>COMPANY</h2>
+            <ul>
+              <li>Home</li>
+              <li>About us</li>
+              <li>Delivery</li>
+              <li>Privacy policy</li>
+            </ul>
+          </div>
+          <div className="footer-content-right">
+              <h2>GET IN TOUCH</h2>
+              <ul>
+                <li>+1-211-456-7890</li>
+                <li>contact@Fusk-it.com</li>
+              </ul>
+          </div>
+        </div>
+        <hr/>
+        <p className='footer-copyright'>Copyright 2024 © fuckit.com - All Right Reserved</p>
+    </div>
+  )
+}
+
+export default Footer
